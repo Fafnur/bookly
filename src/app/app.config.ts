fdafce9c8307c223862d0d203a1e3caeca8c60da
Aleksandr Serenko <fafnur@yandex.ru>
2024-12-07 22:34:01 +0700

    [config] Change app component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,10 @@ import type { ApplicationConfig } from '@angular/core';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideCurrency } from '@bookly/core';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideCurrency('RUD')],
 };
