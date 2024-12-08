57561bae823254a84b5740d432e419f6098c888c
Aleksandr Serenko <fafnur@yandex.ru>
2024-12-08 13:24:32 +0700

    [ui][home] Generating home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import type { Routes } from '@angular/router';
 
+import { HomePageComponent } from '@bookly/home/page';
 import { LayoutComponent } from '@bookly/ui/layout';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    children: [],
+    children: [
+      {
+        path: '',
+        component: HomePageComponent,
+      },
+    ],
   },
 ];
