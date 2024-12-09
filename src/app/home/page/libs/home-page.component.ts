bf872c320ed379705b1f5e7a4fbd67883a2e6be2
Aleksandr Serenko <fafnur@yandex.ru>
2024-12-09 12:13:22 +0700

    [home] Change home

--- src/app/home/page/libs/home-page.component.ts
+++ src/app/home/page/libs/home-page.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
+import { ContainerComponent } from '@bookly/ui/container';
+
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ContainerComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
