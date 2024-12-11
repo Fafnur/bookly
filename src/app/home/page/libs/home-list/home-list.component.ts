507cd636a7049b14428b9599596194710dec350d
Aleksandr Serenko <fafnur@yandex.ru>
2024-12-12 00:25:35 +0700

    [books] Create templates samples

--- src/app/home/page/libs/home-list/home-list.component.ts
+++ src/app/home/page/libs/home-list/home-list.component.ts
@@ -1,3 +1,4 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 
 import type { Book } from '@bookly/books/common';
@@ -6,7 +7,7 @@ import { HomeBookComponent } from './home-book/home-book.component';
 
 @Component({
   selector: 'app-home-list',
-  imports: [HomeBookComponent],
+  imports: [HomeBookComponent, NgTemplateOutlet],
   templateUrl: './home-list.component.html',
   styleUrl: './home-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
